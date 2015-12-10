fdbba3db389b62af1f2754e157d51da9cff7b076
jess <jbrachtie@gmail.com>
2015-12-10 00:19:46 -0500

    fix sidenav i guess

--- www/index.js
+++ www/index.js
@@ -1,4 +1,8 @@
 // close sidenav when clicked
 $('.side-nav li a').on("click", function(e) {
 	$('.button-collapse').sideNav('hide');
+	var overlay = document.getElementById("sidenav-overlay");
+	var overlayParent = overlay.parentNode;
+	overlayParent.removeChild(overlay);
+	$('.button-collapse').sideNav('hide');
 });
