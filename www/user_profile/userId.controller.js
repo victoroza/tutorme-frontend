33d093793685062907dce4e72ae561fb79a066b2
Victor Oza <Victor Oza>
2015-12-09 20:13:18 -0500

    started to work on user profile

--- www/user_profile/userId.controller.js
+++ www/user_profile/userId.controller.js
@@ -5,25 +5,25 @@
         .module('app')
         .controller('UserIdController', UserIdController);
 
-    UserIdController.$inject = ['$location', 'AuthenticationService', '$scope', '$http'];
-    function UserIdController($location, AuthenticationService, $scope, $http) {
+    UserIdController.$inject = ['$location', 'AuthenticationService', '$scope', '$http', '$routeParams'];
+    function UserIdController($location, AuthenticationService, $scope, $http, $routeParams) {
         var vm = this;
         console.log("getting user profile");
         (function initController() {
-            //  $http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/'+ 'victor' +'?format=json').then(function(data) {
-            //     console.log("getting user profile");
-            //     $scope.user = data.data;
-            //     console.log(data.data);
-            // });
+             $http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/'+ $routeParams.userId +'?format=json').then(function(data) {
+                console.log("getting user profile");
+                $scope.user = data.data;
+                console.log(data.data);
+            });
         })();
 
-        // function populateData() {
-        //     console.log("getting user profile");
-        //     vm.dataLoading = true;
-        //      $http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/'+ 'victor' +'?format=json').then(function(data) {
-        //         $scope.user = data.data;
-        //     });
-        // };
+        function populateData() {
+            console.log("getting user profile");
+            vm.dataLoading = true;
+             $http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/'+ $routeParams.userId +'?format=json').then(function(data) {
+                $scope.user = data.data;
+            });
+        };
     }
 
 })();
