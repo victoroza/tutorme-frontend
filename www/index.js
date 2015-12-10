b163b4e0dc0a80c48c21cf72da47f45ae08e62d3
nighthawk1020 <nighthawk1020@gmail.com>
2015-12-09 23:21:30 -0500

    fixed colors fuckerssss

--- www/index.js
+++ www/index.js
@@ -6,3 +6,8 @@ $('.side-nav li a').on("click", function(e) {
 	overlayParent.removeChild(overlay);
 	$('.button-collapse').sideNav("hide");
 });
+$('sideNav').on("hide", function(e){
+	var overlay = document.getElementById("sidenav-overlay");
+	var overlayParent = overlay.parentNode;
+	overlayParent.removeChild(overlay);
+})
