87da56b9af2100d682e64226b3cd10dd73c41801
nighthawk1020 <nighthawk1020@gmail.com>
2015-12-12 01:21:16 -0500

    added user profile shit

--- www/users/users.controller.js
+++ www/users/users.controller.js
@@ -1,22 +1,3 @@
-// (function () {
-//     'use strict';
-
-//     angular
-//         .module('app')
-//         .controller('UserController', UserController);
-
-//     UserController.$inject = ['$location'];
-//     function UserController($scope, $location) {
-//         $http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/?format=json').then(function(data) {
-//         $scope.users = data.data;
-//     });
-
-// });
-// var app = angular.module('app', []).controller('UserController', function($scope, $http) {
-//     $http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/?format=json').then(function(data) {
-//         $scope.users = data.data;
-//     });
-// });
 (function () {
     'use strict';
 
