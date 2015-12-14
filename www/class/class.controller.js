0f2d78f331de85f7521c36661d739cfeee9ba3e4
jess <jbrachtie@gmail.com>
2015-12-14 03:14:26 -0500

    add classes, order, refresh

--- www/class/class.controller.js
+++ www/class/class.controller.js
@@ -5,8 +5,8 @@
 		.module('app')
 		.controller('ClassController', ClassController);
 
-	ClassController.$inject = ['$location', 'AuthenticationService', '$scope', '$http', '$routeParams', 'classGetService'];
-	function ClassController($location, AuthenticationService, $scope, $http, $routeParams, classGetService) {
+	ClassController.$inject = ['$location', 'AuthenticationService', '$scope', '$http', '$routeParams', 'classGetService', '$route'];
+	function ClassController($location, AuthenticationService, $scope, $http, $routeParams, classGetService, $route) {
 		var vm = this;
 		(function initController() {
 			// $scope.classes1 = null;
@@ -31,14 +31,35 @@
 		}
 
 		$scope.addClass = function(){
+			$('#modal1').closeModal();
 			vm.dataLoading = true;
 			console.log("sup");
 			console.log($routeParams);
+			getSchoolId($routeParams.schoolId);
+		}
+
+		function getSchoolId(schoolName) {
+			$http.get('http://tutorme-backend.herokuapp.com/tutor_api/schools/?name=' + schoolName + '&format=json')
+				.then(function(response) {
+					var schoolNumber = (response.data)[0].id;
+					getMajorNumber(schoolNumber);
+				});
+		}
+
+		function getMajorNumber(schoolNumber) {
+			$http.get('http://tutorme-backend.herokuapp.com/tutor_api/departments/?shortName=' + vm.major + '&format=json')
+				.then(function(response) {
+					var majorNumber = (response.data)[0].id;
+					postNewClass(schoolNumber, majorNumber);
+				});
+		}
+
+		function postNewClass(s, m) {
 			$http.post("http://tutorme-backend.herokuapp.com/tutor_api/classes/", {
 				number: vm.classNumber,
-				major: vm.major,
-				school: $routeParams.schoolId
-			});
+				major: m,
+				school: s
+			}).then(function(r) { $route.reload() });
 		}
 
 	}
