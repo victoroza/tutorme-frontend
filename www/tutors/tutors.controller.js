48f706b0eadb3cf199180844d8b356e84b99f0df
Victor Oza <Victor Oza>
2015-12-14 02:02:53 -0500

    added add as tutor

--- www/tutors/tutors.controller.js
+++ www/tutors/tutors.controller.js
@@ -5,8 +5,8 @@
 		.module('app')
 		.controller('TutorController', TutorController);
 
-	TutorController.$inject = ['$location', 'AuthenticationService', '$scope', '$http', '$routeParams'];
-	function TutorController($location, AuthenticationService, $scope, $http, $routeParams) {
+	TutorController.$inject = ['$location', 'AuthenticationService', '$scope', '$http', '$routeParams', '$route'];
+	function TutorController($location, AuthenticationService, $scope, $http, $routeParams, $route) {
 		var vm = this;
 		(function initController() {
 
@@ -34,7 +34,31 @@
 
 
 		})(); //end initController
-
+		$scope.addToTutor = function addToTutor(){
+				$http.get('http://tutorme-backend.herokuapp.com/tutor_api/tutor/?aClass__number=' + $routeParams.classNumber + '&user__username=' + localStorage.username +'&format=json').then(function(response){
+						if(response.data.length > 0){
+							$('#modal1').openModal();
+						} else {
+							postAddToTutor();
+						}
+					}); //end get
+			}
+		function postAddToTutor() {
+			$http.post('http://tutorme-backend.herokuapp.com/tutor_api/tutor/', {
+                aClass : $routeParams.classId,
+                user : localStorage.id,
+	            })
+	                .then(function(response) {
+	                    // alert(response.status);
+	                    console.log(response);
+	                    console.log(response.data);
+	                    $route.reload();
+	                },
+	                function(response) {
+	                    alert("There is an error in the input");
+	                    // alert(response.status);
+	                });
+		}
 	} //end TutorController
 
 })();
