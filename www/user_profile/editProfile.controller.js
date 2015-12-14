a5c5db50bc3da1f6cc284453a8bd08a9136c759f
jess <jbrachtie@gmail.com>
2015-12-13 21:13:46 -0500

    list what classes you're a tutor for on your own profile

--- www/user_profile/editProfile.controller.js
+++ www/user_profile/editProfile.controller.js
@@ -1,41 +1,65 @@
 (function () {
-    'use strict';
+	'use strict';
 
-    angular
-        .module('app')
-        .controller('EditProfileController', EditProfileController);
+	angular
+		.module('app')
+		.controller('EditProfileController', EditProfileController);
 
-    EditProfileController.$inject = ['$location', 'AuthenticationService', '$scope', '$http', '$routeParams'];
-    function EditProfileController($location, AuthenticationService, $scope, $http, $routeParams) {
-        var config = { 'headers': {'Authorization': 'Token ' + localStorage.token}};
-        var vm = this;
-        console.log("editing profile");
-        (function initController() {
-             $http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/'+ $routeParams.userId +'?format=json', config).then(function(data) {
-                $scope.user = data.data;
-            });
-        })();
+	EditProfileController.$inject = ['$location', 'AuthenticationService', '$scope', '$http', '$routeParams'];
+	function EditProfileController($location, AuthenticationService, $scope, $http, $routeParams) {
+		var config = { 'headers': {'Authorization': 'Token ' + localStorage.token}};
+		var vm = this;
+		var classes = [];
+		console.log("editing profile");
+		(function initController() {
+			$http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/'+ $routeParams.userId +'?format=json', config).then(function(data) {
+				$scope.user = data.data;
+				$scope.classes = classes;
+				getTutorClasses(data.data);
+			});
 
-        function populateData() {
-            vm.dataLoading = true;
-             $http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/'+ $routeParams.userId +'?format=json', config).then(function(data) {
-                console.log("got to populateData editProfile");
-                $scope.user = data.data;
-            });
-        };
+			function getTutorClasses(user) {
+				$http.get('http://tutorme-backend.herokuapp.com/tutor_api/tutor/?user__username=' + user.username + '&format=json')
+				.then(function(response) {
+					loopClasses(response.data);
+				});
+			}
 
-        function update() {
-          vm.dataLoading = true;
-          console.log(vm);
-          $http.put('http://tutorme-backend.herokuapp.com/tutor_api/users/' + $routeParams.userId, {
-              first_name: vm.first_name,
-              last_name: vm.last_name,
-              email: vm.email,
-              phone: vm.phone,
-              password: vm.password,
-              username: localStorage.username,
-              picture: vm.picture
-            })
-        };
-    }
+			function loopClasses(classList) {
+				$.each(classList, function(i, c) {
+					$http.get('http://tutorme-backend.herokuapp.com/tutor_api/classes/?id=' + c.aClass + '&format=json')
+					.then(function(response) {
+						setClassInfo(response.data);
+					});
+				});
+			}
+
+			function setClassInfo(classInfo) {
+				classes.push(classInfo[0]);
+			}
+		})();
+
+		function populateData() {
+			vm.dataLoading = true;
+			$http.get('http://tutorme-backend.herokuapp.com/tutor_api/users/'+ $routeParams.userId +'?format=json', config).then(function(data) {
+				console.log("got to populateData editProfile");
+				$scope.user = data.data;
+			});
+		};
+
+		function update() {
+			vm.dataLoading = true;
+			console.log(vm);
+			$http.put('http://tutorme-backend.herokuapp.com/tutor_api/users/' + $routeParams.userId, {
+				first_name: vm.first_name,
+				last_name: vm.last_name,
+				email: vm.email,
+				phone: vm.phone,
+				password: vm.password,
+				username: localStorage.username,
+				picture: vm.picture
+			})
+		};
+
+	}
 })();
