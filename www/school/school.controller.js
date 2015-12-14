fd4992a066032a64aef26208708f997bebb66101
nighthawk1020 <nighthawk1020@gmail.com>
2015-12-14 00:36:28 -0500

    show new notifications

--- www/school/school.controller.js
+++ www/school/school.controller.js
@@ -9,6 +9,18 @@
     function SchoolController($location, AuthenticationService, $scope, $http) {
         var vm = this;
         (function initController() {
+          $http.get("http://tutorme-backend.herokuapp.com/tutor_api/appointments/?tutor__username=" + localStorage.username + "&confirmed_tutor=False&format=json")
+                .then(function(response) {
+                  console.log("here!");
+                  var data = response.data.length;
+                  if(data > 0){
+                    console.log("hello");
+                    $('.badge').text(data);
+                  }else{
+                    console.log("your love is so toxic");
+                    $('.badge').attr('display', 'none');
+                  }
+                });
             $http.get('http://tutorme-backend.herokuapp.com/tutor_api/schools/?format=json').then(function(data) {
                 $scope.schools = data.data;
             });
